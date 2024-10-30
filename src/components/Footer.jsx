ab7cb68ef457a01860d7065a51c12c15872f28a0
Shubham Sharma <160613090+onlyshubham4u@users.noreply.github.com>
2024-10-30 14:14:42 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
       <footer className="mb-0 text-center">
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
-            <p className="mb-3 mb-md-0">Made with ❤️ by Shubham Sharma
-              <a  href="#" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Sahib Singh</a>
+            <p className="mb-3 mb-md-0">Made with ❤️ by 
+              <a  href="#" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Shubham Sharma</a>
             </p>
             <a className="text-dark fs-4" href="https://github.com/onlyshubham4u" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
